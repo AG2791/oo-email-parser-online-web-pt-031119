c01ceb280a2f89e2d1db97ef2fbdb0da06b43ae5
AJ Guiles <ashiguiles@gmail.com>
2019-04-08 04:11:10 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -9,15 +9,10 @@ class EmailParser
   def initialize(emails)
 		@emails= emails
 	end
-  def initialize(emails)
-    emails.split(/[\s,]/)
-    @emails.uniq
-  end
- 
+  
 	def parse
 		@emails = @emails.gsub(/[\s,]/ ," ").split
-		# @email is an array now
 		@emails.uniq
-	
+	end
   
 end
